3de210339d5040ae853b9906f16be62c9b1e7a59
Khngan <phankhanhnganngan@gmail.com>
2022-10-02 16:58:23 +0700

    🌈 Implement sending password reset email and welcome email using sendGrid

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,7 +4,7 @@ const { promisify } = require('util');
 const catchAsync = require('../utils/catchAsync');
 const jwt = require('jsonwebtoken');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -48,6 +48,11 @@ exports.signup = catchAsync(async (req, res, next) => {
     role: req.body.role
   });
   await newUser.save();
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log(url);
+  await new Email(newUser, url).sendWelcome();
+
   createSendToken(newUser, 201, res);
 });
 
@@ -175,20 +180,14 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   // 2) Generate random refresh token
   const resetToken = user.createPasswordResetToken(); //modify some fields but not yet saved
   await user.save({ validateBeforeSave: false });
+
   // 3) Send it to user's email
-
-  const resetURL = `${req.protocol}://${req.get(
-    'host'
-  )}/api/v1/users/resetPassword}/${resetToken}`;
-
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to: ${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
-
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min).',
-      message
-    });
+    const resetURL = `${req.protocol}://${req.get(
+      'host'
+    )}/api/v1/users/resetPassword/${resetToken}`;
+
+    await new Email(user, resetURL).sendPasswordReset();
 
     res.status(200).json({
       status: 'success',
--- utils/email.js
+++ utils/email.js
@@ -1,28 +1,69 @@
 const nodemailer = require('nodemailer');
-const catchAsync = require('../utils/catchAsync');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
+const path = require('path');
 
-const sendEmail = async options => {
-  // 1) create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Khanh Ngan Phan <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'production') {
+      // send grid
+      return nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USERNAME,
+          pass: process.env.SENDGRID_PASSWORD
+        }
+      });
     }
-  });
 
-  // 2) define email options
-  const mailOptions = {
-    from: 'Khanh Ngan Phan <phankhanhngan@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+    // 1) create a transporter
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD
+      }
+    });
+  }
 
-  // 3) actually send the email
-  await transporter.sendMail(mailOptions); //return a promise
+  async send(template, subject) {
+    // 1) render html based on a pug template
+    const filePath = path.join(__dirname, '..', 'views', `${template}.pug`);
+    const html = pug.renderFile(filePath, {
+      firstName: this.firstName,
+      url: this.url,
+      subject
+    });
+
+    // 2) Define the email options
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.fromString(html)
+    };
+
+    // 3) Create a transport and send email
+    await this.newTransport().sendMail(mailOptions);
+  }
+
+  async sendWelcome() {
+    await this.send('welcome', 'Welcome to the Natours family!');
+  }
+
+  async sendPasswordReset() {
+    await this.send(
+      'passwordReset',
+      'Your password reset token (valid for only 10 minutes'
+    );
+  }
 };
-
-module.exports = sendEmail;
